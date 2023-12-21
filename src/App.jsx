03caf594b443acdc472787e6ab8b3e34da936e17
Mujib <mujib@Mujibs-MacBook-Pro.local>
2023-12-21 09:09:34 +0530

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import Header from "./Header";
-import Footer from "./Footer";
-import Display from "./Display";
-import Names from "./Names";
-import Name from "./Name";
-import PI, { add as add2, obj } from './math';
+import ProductList from "./ProductList";
 
 function App() {
     return <div>
-        <h1>{add2(100, 200)}</h1>
-        <Header></Header>
-        <Names />
-        <Name value="Uzair" designation="SSE" />
-        <Display />
-        <Footer></Footer>
+        <Header />
+        <ProductList />
     </div>;
 }
 
--- src/Display.jsx
+++ src/Display.jsx
@@ -1,13 +1,15 @@
-import React from "react"
+import React from "react";
+import './Display.css';
 
 function Display() {
-    // <img src="img.jpeg" width="100" height="100"/>
-    return React.createElement('img',
-        {
-            src: 'https://images.pexels.com/photos/9367332/pexels-photo-9367332.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
-            width: 200,
-            height: 200
-        }, null);
+    // inline styles
+    // <img style="border-radius:10px" src="img.jpeg" width="100" height="100"/>
+    return <div>
+        <h1 className="heading">Hello</h1>
+        <img className="img" src="https://images.pexels.com/photos/9367332/pexels-photo-9367332.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            width="200" height="200" />
+    </div>
+
 }
 
 export default Display;
