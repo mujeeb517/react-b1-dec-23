4834a620adf1a08b95e14a5f07228f435bff4f85
Mujib <mujib@Mujibs-MacBook-Pro.local>
2024-01-12 08:47:29 +0530

    summary

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,3 +21,33 @@ module.exports = {
 // uglification
 // js bundle 
 // css bundle
+
+
+/* 
+1. Introduction
+2. createElement
+3. JSX
+4. component (function/class, input, output, state)
+5. conditional rendering
+6. render lists
+7. class
+8. state, setState
+9. migration
+10. axios, async 
+11. paginatation
+12. views 
+13. searching
+14. sorting
+15. product detail 
+16. Routing, SPA
+17. Life cycle methods
+18. Hooks
+19. build 
+20. deploy 
+21. Add product, forms, validations (formik)
+
+
+22. Calculator
+23. Cart 
+24. Reviews
+*/
